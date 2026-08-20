aaa3de05bf651f20fc71a843f1cf57d6b8fcc1b5
Yanari Taeko <tae.ienari@gmail.com>
2026-08-20 17:02:56 +0900

    feat: ジャンル登録機能を実装

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreGenreRequest;
 
 class GenreController extends Controller
 {
@@ -23,5 +24,22 @@ class GenreController extends Controller
         ->paginate(10);
 
         return view('genres.show', compact('genre','books'));
+    }
+
+    public function create()
+    {
+        return view('genres.create');
+    }
+
+    public function store(StoreGenreRequest $request)
+    {
+        try {
+            $validated = $request->validated();
+            $genre = Genre::create($validated);
+
+            return redirect()->route('genres.index')->with('success', 'ジャンルを登録しました');
+        } catch (\Throwable $exception) {
+            return redirect()->back()->withInput()->with('error', 'ジャンルの登録に失敗しました');
+        }
     }
 }
---
+++ app/Http/Requests/StoreGenreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreGenreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:20', 'unique:genres,name'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'ジャンルの入力は必須です。',
+            'name.string' => '文字列で入力してください。',
+            'name.max' => 'ジャンルは20文字以内で入力してください。',
+            'name.unique' => 'このジャンル名は重複しています。',
+        ];
+    }
+}
